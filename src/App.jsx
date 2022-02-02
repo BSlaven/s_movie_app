6c8d115c4cb62cd1caace2c25451c21ecdfd3b44
Slaven B <slaven.bunijevac@gmail.com>
2022-02-02 23:33:55 +0100

    change app name in index.html file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import classes from './App.module.css'
 
 function App() {
 
   return (
-    <div className="App">
-      
+    <div className={classes.app}>
+
     </div>
   )
 }
 
-export default App
+export default App;
