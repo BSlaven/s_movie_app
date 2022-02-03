0973d2e922dc97c149366bad9e436976a5ad0c36
Slaven B <slaven.bunijevac@gmail.com>
2022-02-03 07:20:41 +0100

    add react-router to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import classes from './App.module.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import classes from './App.module.css';
 
 function App() {
 
   return (
     <div className={classes.app}>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          {/* <Route path="/:id" element={<Movie />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
