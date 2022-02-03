db3c3e43673a9a04b0280c20322f4ac4b2377ef0
Slaven B <slaven.bunijevac@gmail.com>
2022-02-03 09:03:09 +0100

    introduce base fetch url

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,16 +2,22 @@ import { useState, useEffect } from 'react';
 
 import classes from './Movies.module.css';
 
+const MY_API_KEY = import.meta.env.VITE_APP_API_KEY;
+
 const Movies = () => {
 
   const [ movies, setMovies ] = useState([]);
+
+  const fetchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${MY_API_KEY}`
   
-  // useEffect(() => {
-  //   fetchMovies();
-  // }, [])
+  useEffect(() => {
+    fetchMovies(fetchUrl);
+  }, [])
   
-  const fetchMovies = url => {
-    
+  const fetchMovies = async url => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results);
   }
 
   return (
