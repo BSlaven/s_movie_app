bd9c2c3700ad66d359f8d82edaa45a8805f51c18
Slaven B <slaven.bunijevac@gmail.com>
2022-02-03 09:21:54 +0100

    introduce MovieCard component and its corresponding css file

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,9 @@
+const MovieCard = () => {
+  return (
+    <div>
+      This is a MovieCard component
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -7,24 +7,27 @@ const MY_API_KEY = import.meta.env.VITE_APP_API_KEY;
 const Movies = () => {
 
   const [ movies, setMovies ] = useState([]);
+  // cosnt [ page, setPage ] = useState(1);
 
-  const fetchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${MY_API_KEY}`
+  const baseFetchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${MY_API_KEY}`
+
+  // const searchFetchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${MY_API_KEY}`
   
-  useEffect(() => {
-    fetchMovies(fetchUrl);
-  }, [])
+  // useEffect(() => {
+  //   fetchMovies(baseFetchUrl);
+  // }, [])
   
-  const fetchMovies = async url => {
+  const fetchMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.results);
+    setMovies(data.results);
   }
 
   return (
     <div className={classes.movies}>
       <h2>All your movies right here</h2>
       <div className={classes.moviesContainer}>
-
+        
       </div>
     </div>
   );
